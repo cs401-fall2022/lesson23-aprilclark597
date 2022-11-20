389dad0bae118c7ad9d852fcc1e19786e37cc195
April Clark <aprilclark597@u.boisestate.edu>
2022-11-20 16:01:23 -0700

    progress has been made

---
+++ routes/index.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var router = express.Router();
+const sqlite3 = require('sqlite3').verbose()
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  var db = new sqlite3.Database('mydb.sqlite3',
+    sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+    (err) => {
+      if (err) {
+        console.log("Getting error " + err);
+        exit(1);
+      }
+      //Query if the table exists if not lets create it on the fly!
+      db.all(`SELECT blog FROM sqlite_master WHERE type='table' AND name='blog'`,
+        (err, rows) => {
+          if (rows.length === 1) {
+            console.log("Table exists!");
+            db.all(` select blog_id, title from blogEntries`, (err, rows) => {
+              console.log("returning " + rows.length + " records");
+              res.render('index', { title: 'Blog Database', data: rows });
+            });
+          } else {
+            console.log("Creating table and inserting some sample data");
+            db.exec(`create table blog (
+                     blog_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                     title text NOT NULL);
+                      insert into blog (title)
+                      values ('Welcome to BlahBlahBlog'),
+                             ('When to Water');`,
+              () => {
+                db.all(`select blog_id, title from blog`, (err, rows) => {
+                  res.render('index', { title: 'Blog Database', data: rows });
+                });
+              });
+          }
+        });
+    });
+});
+
+router.post('/add', (req, res, next) => {
+  var db = new sqlite3.Database('mydb.sqlite3',
+    sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+    (err) => {
+      if (err) {
+        console.log("Getting error " + err);
+        exit(1);
+      }
+      console.log("inserting " + req.body.blog);
+      db.exec("insert into blog (title) values (?)", [req.body.blog]);
+      //redirect to homepage
+      res.redirect('/');
+    }
+  );
+})
+
+router.post('/delete', (req, res, next) => {
+  var db = new sqlite3.Database('mydb.sqlite3',
+    sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+    (err) => {
+      if (err) {
+        console.log("Getting error " + err);
+        exit(1);
+      }
+      console.log("deleting " + req.body.blog);
+      db.run("delete from blog where blog_id= ?", [req.body.blog]);     
+      res.redirect('/');
+    }
+  );
+})
+
+module.exports = router;
