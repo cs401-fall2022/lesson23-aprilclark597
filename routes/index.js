3df4b63252a11568a5e269c9fbcd021e0830a414
April Clark <aprilclark597@u.boisestate.edu>
2022-11-24 18:23:44 -0700

    way prettier

--- routes/index.js
+++ routes/index.js
@@ -12,25 +12,26 @@ router.get('/', function (req, res, next) {
         exit(1);
       }
       //Query if the table exists if not lets create it on the fly!
-      db.all(`SELECT blog FROM sqlite_master WHERE type='table' AND name='blog'`,
+      db.all(`SELECT name FROM sqlite_master WHERE type='table' AND name='blog'`,
         (err, rows) => {
           if (rows.length === 1) {
             console.log("Table exists!");
-            db.all(` select blog_id, title from blogEntries`, (err, rows) => {
+            db.all(` select blog_id, blog_txt from blog`, (err, rows) => {
               console.log("returning " + rows.length + " records");
-              res.render('index', { title: 'Blog Database', data: rows });
+              res.render('index', { title: 'BlahBlahBlog', data: rows });
             });
           } else {
             console.log("Creating table and inserting some sample data");
             db.exec(`create table blog (
                      blog_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                     title text NOT NULL);
-                      insert into blog (title)
+                     blog_txt text NOT NULL);
+
+                      insert into blog (blog_txt)
                       values ('Welcome to BlahBlahBlog'),
                              ('When to Water');`,
               () => {
-                db.all(`select blog_id, title from blog`, (err, rows) => {
-                  res.render('index', { title: 'Blog Database', data: rows });
+                db.all(` select blog_id, blog_txt from blog`, (err, rows) => {
+                  res.render('index', { title: 'BlahBlahBlog', data: rows });
                 });
               });
           }
@@ -39,6 +40,7 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/add', (req, res, next) => {
+  console.log("Adding blog to table without sanitizing input!");
   var db = new sqlite3.Database('mydb.sqlite3',
     sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
     (err) => {
@@ -47,7 +49,9 @@ router.post('/add', (req, res, next) => {
         exit(1);
       }
       console.log("inserting " + req.body.blog);
-      db.exec("insert into blog (title) values (?)", [req.body.blog]);
+      //NOTE: This is dangerous! you need to sanitize input from the user
+      db.exec(`insert into blog ( blog_txt)
+                values ('${req.body.blog}');`)
       //redirect to homepage
       res.redirect('/');
     }
@@ -55,6 +59,7 @@ router.post('/add', (req, res, next) => {
 })
 
 router.post('/delete', (req, res, next) => {
+  console.log("deleting stuff without checking if it is valid! SEND IT!");
   var db = new sqlite3.Database('mydb.sqlite3',
     sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
     (err) => {
@@ -62,8 +67,9 @@ router.post('/delete', (req, res, next) => {
         console.log("Getting error " + err);
         exit(1);
       }
-      console.log("deleting " + req.body.blog);
-      db.run("delete from blog where blog_id= ?", [req.body.blog]);     
+      console.log("inserting " + req.body.blog);
+      //NOTE: This is dangerous! you need to sanitize input from the user
+      db.exec(`delete from blog where blog_id='${req.body.blog}';`);     
       res.redirect('/');
     }
   );
